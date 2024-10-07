3ccadb68201acba22f1c9bfeb8ed1777ed2f7c42
CarlosMuLa <116656412+CarlosMuLa@users.noreply.github.com>
2024-10-07 14:54:38 -0600

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+#----- Configuración Inicial del Panel Central --------------------
+st.title("Mexico Crime Rates")
+st.sidebar.title("Menu")
